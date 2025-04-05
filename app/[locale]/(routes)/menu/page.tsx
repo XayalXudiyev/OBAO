8ea89e060c734ebba707572140ec090db5789db2
Xayalxudiyev <xayal.xuduyev@gmail.com>
2025-04-05 18:12:31 +0400

    Refactor menu structure by defining MenuItem and FoodItem interfaces, and implement a comprehensive menu data array. Update FoodMenuPage to utilize the new menu data, enhancing the display of menu items with a carousel layout for better user experience.

--- app/[locale]/(routes)/menu/page.tsx
+++ app/[locale]/(routes)/menu/page.tsx
@@ -1,15 +1,24 @@
 "use client"
 
-// import OfferMealComponent from "@/app/(routes)/menu/_compnents/OfferMealComponent"
 import { Button } from "@/components/ui/button"
-import { categories } from "@/constans"
+import { categories, menu } from "@/constans"
 import { cn } from "@/lib/utils"
-// import Image from "next/image"
 import React, { useState } from "react"
 import BottomMealComponent from "./_compnents/BottomMealComponent"
 import DiscoverFlavors from "./_compnents/DiscoverFlavors"
 import MidMealComponent from "./_compnents/MidMealComponent"
 import TopMealComponent from "./_compnents/TopMealComponent"
+import OfferMealComponent from "./_compnents/OfferMealComponent"
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import Image from "next/image"
+import { DotIcon } from "lucide-react"
 
 const FoodMenuPage = () => {
   const [clicked, setClicked] = useState<number>(1)
@@ -18,35 +27,126 @@ const FoodMenuPage = () => {
     setClicked(id)
   }
 
+  const chunkArray = (array: any[], size: number) => {
+    const result = []
+    for (let i = 0; i < array.length; i += size) {
+      result.push(array.slice(i, i + size))
+    }
+    return result
+  }
+
   return (
     <div className="relative flex bg-[#1C1C1C]">
-      <div className="absolute top-0 left-0 w-full h-full bg-[url('/cropped.png')] bg-center bg-[length:120%_auto]  bg-repeat-y opacity-10 z-10" />
+      <div className="absolute top-0 left-0 w-full h-full bg-[url('/cropped.png')] bg-center bg-[length:120%_auto] bg-repeat-y opacity-10 z-10" />
 
-      <div className="z-20 text-white  flex flex-col justify-center items-center w-full relative">
-        {/* <Image
-          width={1}
-          height={1}
-          src="/menuHero.svg"
-          alt="menuHero"
-          className="w-full py-10 px-1 md:px-48 md:py-28"
-        /> */}
+      <div className="z-20 text-white flex flex-col justify-center items-center w-full relative">
         <DiscoverFlavors />
 
         <div className="w-full">
           <h2 className="text-3xl md:text-4xl font-bold font-avenirMedium4 text-center mt-5 md:mt-20 md:mb-10">
             OUR OFFERINGS
           </h2>
-          {/* {[1, 2].map((_, index) => (
-            <OfferMealComponent
-              key={index}
-              isImageLeft={index % 2 === 1}
-              selectedCategory={String(clicked)}
-            />
-          ))} */}
 
-          <TopMealComponent selectedCategory={String(clicked)} />
-          <MidMealComponent selectedCategory={String(clicked)} />
-          <BottomMealComponent selectedCategory={String(clicked)} />
+          {menu.map((item, index) => {
+            const itemChunks = chunkArray(item.items, 3)
+            const isOdd = (index + 1) % 2 !== 0
+            return (
+              <div
+                className="flex w-full justify-center items-center mb-10 px-[72px]"
+                key={index}
+              >
+                {!isOdd ? (
+                  <>
+                    <div className="w-1/2 flex">
+                      <Image
+                        src={item.image}
+                        alt="menu"
+                        width={10000}
+                        height={10000}
+                        className="object-cover"
+                      />
+                    </div>
+                    <div className="w-1/2">
+                      <Carousel className="w-full h-full">
+                        <h2 className="text-xl font-bold mb-4">{item.title}</h2>
+                        <CarouselContent>
+                          {itemChunks.map((chunk, chunkIndex) => (
+                            <CarouselItem key={chunkIndex}>
+                              <div className="flex flex-col space-y-4">
+                                {chunk.map((foodItem: any, idx: number) => (
+                                  <div
+                                    key={idx}
+                                    className="flex flex-col text-sm justify-center"
+                                  >
+                                    <div className="flex justify-between">
+                                      <h3 className="flex items-center">
+                                        <DotIcon className="size-8 text-red-500" />
+                                        {foodItem.name}
+                                      </h3>
+                                      <p>{foodItem.price}</p>
+                                    </div>
+                                    <p className="text-gray-500 ml-8">
+                                      {foodItem.description || foodItem.content}
+                                    </p>
+                                  </div>
+                                ))}
+                              </div>
+                            </CarouselItem>
+                          ))}
+                        </CarouselContent>
+                        <CarouselPrevious />
+                        <CarouselNext />
+                      </Carousel>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <div className="w-1/2">
+                      <Carousel className="w-full h-full">
+                        <h2 className="text-xl font-bold mb-4">{item.title}</h2>
+                        <CarouselContent>
+                          {itemChunks.map((chunk, chunkIndex) => (
+                            <CarouselItem key={chunkIndex}>
+                              <div className="flex flex-col space-y-4">
+                                {chunk.map((foodItem: any, idx: number) => (
+                                  <div
+                                    key={idx}
+                                    className="flex flex-col text-sm justify-center"
+                                  >
+                                    <div className="flex justify-between">
+                                      <h3 className="flex items-center">
+                                        <DotIcon className="size-8 text-red-500" />
+                                        {foodItem.name}
+                                      </h3>
+                                      <p>{foodItem.price}</p>
+                                    </div>
+                                    <p className="text-gray-500 ml-8">
+                                      {foodItem.description || foodItem.content}
+                                    </p>
+                                  </div>
+                                ))}
+                              </div>
+                            </CarouselItem>
+                          ))}
+                        </CarouselContent>
+                        <CarouselPrevious />
+                        <CarouselNext />
+                      </Carousel>
+                    </div>
+                    <div className="w-1/2 flex">
+                      <Image
+                        src={item.image}
+                        alt="menu"
+                        width={10000}
+                        height={10000}
+                        className="object-cover"
+                      />
+                    </div>
+                  </>
+                )}
+              </div>
+            )
+          })}
         </div>
       </div>
     </div>
--- constans/index.ts
+++ constans/index.ts
@@ -1,5 +1,19 @@
 import type { Category, RouteProps } from "./types"
 
+interface MenuItem {
+  title: string;
+  image: string;
+  items: FoodItem[];
+}
+
+interface FoodItem {
+  id: number;
+  name: string;
+  price: string;
+  content?: string;
+  description?: string;
+}
+
 export const routes: RouteProps[] = [
   { id: 0, href: "/", title: "Home" },
   { id: 1, href: "/menu", title: "Menu" },
@@ -29,4 +43,195 @@ export const categories: Category[] = [
     id: 5,
     title: "Non gluten",
   },
+]
+
+
+
+export const menu: MenuItem[] = [
+  {
+    title: "POTAGE",
+    image: "/menu/POTAGE.webp",
+    items: [
+      {
+        id: 1,
+        name: "Miso soup",
+        price: "5",
+        content: "Scampia, Moules, Crab"
+      },
+      {
+        id: 2,
+        name: "Miso Soup avec Fruit de Mer",
+        price: "17",
+        content: "Scampia, Moules, Crab"
+      }
+    ]
+  },
+
+  {
+    title: "MAKI 6 PCS",
+    image: "/menu/MAKI.webp",
+    items: [
+      {
+        id: 1,
+        name: "Maki Saumon",
+        price: "6.20",
+        description: ""
+      },
+      {
+        id: 2,
+        name: "Maki Thon Spicy Concombre (8pcs)",
+        price: "9.80",
+        description: "Thon tartare, Concombre"
+      },
+      {
+        id: 3,
+        name: "Maki Concombre ",
+        price: "5",
+        description: ""
+      },
+      {
+        id: 4,
+        name: "Maki Avocat",
+        price: "5",
+        description: ""
+      },
+      {
+        id: 5,
+        name: "Futo Maki Végan (8pcs) ",
+        price: "10.50",
+        description: "Japonaise Tofu Marine, Wakame, Concombre, Avocat, Radis Jaune"
+      },
+      {
+        id: 6,
+        name: "Maki Saumon Avocat (8pcs)",
+        price: "9.20",
+        description: ""
+      },
+      {
+        id: 7,
+        name: "O'BAO Futo Maki (8pcs)",
+        price: "12.00",
+        description: "Crab, Omelette Japonaise, Wakame, Masago, Concombre, Avocat, Radis Jaune, Mayo "
+      }
+    ]
+  },
+
+
+  {
+    title: "SASHIMI",
+    image: "/menu/SASHIMI.webp",
+    items: [
+      {
+        id: 1,
+        name: "Saumon (5pcs)",
+        price: "9.50",
+        description: "Tranches de Saumon",
+      },
+      {
+        id: 2,
+        name: "Saumon (5pcs)",
+        price: "10.00",
+        description: "Tranches de Saumon Flambé avec Sauce Japonaise",
+      },
+      {
+        id: 3,
+        name: "Thon (5pcs)",
+        price: "10.50",
+        description: "Tranches de Thon",
+      },
+      {
+        id: 4,
+        name: "Thon Tataki (5pcs) ",
+        price: "11.00",
+        description: "Tranches de Thon Flambé avec Sauce Japonaise",
+      },
+      {
+        id: 5,
+        name: "Deluxe Box (12pcs)",
+        price: "21.50",
+        description: "4pcs Saumon - 4pcs Thon - 4pcs Coquille Saint Jacques ",
+      },
+    ]
+  },
+
+  {
+    title: "SALADES JAPONAISE",
+    image: "/menu/SALADE.webp",
+    items: [
+      {
+        id: 1,
+        name: "Chicken",
+        price: "16.50",
+        description: "Tempura Poulet, Salade, Carotte, Concombre, Tomate, Avocat, Mangue, Radis, Sauce Japonaise, Maïs",
+      },
+      {
+        id: 2,
+        name: "Ebi Tempura",
+        price: "17.50",
+        description: "Tempura Scampis, Salade, Carotte, Concombre, Tomate, Avocat, Mangue, Radis, Sauce Japonaise, Maïs",
+      },
+      {
+        id: 3,
+        name: "Sashimi",
+        price: "18.00",
+        description: "Thon, Saumon, Salade, Carotte, Concombre, Tomate, Avocat, Mangue, Radis, Sauce Wasabi - citrus, Maïs",
+      },
+      {
+        id: 4,
+        name: "Wakame",
+        price: "16.00",
+        description: "Algue Japonaise, Salade, Carrote, Concombre, Tomate, Avocat, Radis, Mangue, Sauce Japonaise, Maïs",
+      },
+      {
+        id: 5,
+        name: "California",
+        price: "16.00",
+        description: "Crab, Masago, Salade, Carotte, Concombre, Tomate, Avocat, Mangue, Radis, Sauce Japonaise, Maïs",
+      },
+      {
+        id: 6,
+        name: "Saumon Fume",
+        price: "19.00",
+        description: "Saumon Fume, Salade, Carotte, Concombre, Tomate, Avocat, Mangue, Radis, Sauce Japonaise, Maïs",
+      },
+    ]
+  },
+
+  {
+    title: "DONBURI BOL A BASE DE RIZ",
+    image: "/menu/DONBURI.webp",
+    items: [
+      {
+        id: 1,
+        name: "Yakitori Poulet",
+        price: "17.00",
+        description: "Morceau de Poulet Grille á la Sauce Japonaise avec Legumes",
+      },
+      {
+        id: 2,
+        name: "Gyuniku",
+        price: "19.00",
+        description: "Morceau de Boeuf Grille á la Sauce Japonaise avec Legumes",
+      },
+      {
+        id: 3,
+        name: "Saumon Grillé",
+        price: "19.00",
+        description: "Saumon Grillé a la Sauce Japonaise avec Brocolis",
+      },
+      {
+        id: 4,
+        name: "Ebi Tempura",
+        price: "18.00",
+        description: "Beignet de Scampis á la Sauce Aigre-Doux ",
+      },
+      {
+        id: 5,
+        name: "Canard Laque",
+        price: "19.00",
+        description: "Canard Laque á la Sauce Hoisin Piquant",
+      }
+    ]
+
+  }
 ]
