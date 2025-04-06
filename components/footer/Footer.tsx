8bb8013de2ca7a23755a22e82bac4d8415927b9c
Xayalxudiyev <xayal.xuduyev@gmail.com>
2025-04-06 20:07:35 +0400

    Update menu layout and content: adjust item chunking in FoodMenuPage, enhance styling for better readability, and update footer to display the current year dynamically. Add new menu items and categories, including ENTREES and URAMAKI, with detailed descriptions and pricing. Replace POTAGE image with a new format.

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -6,8 +6,7 @@ const Footer = () => {
     <footer className="border-t border-[#634927] font-avenirMedium4 text-white ">
       <div className="flex justify-between items-center px-5 sm:px-10 md:px-16 mx-auto  min-h-[76px]">
         <div className="flex text-sm space-x-7">
-          <p>Privacy policy</p>
-          <p>© 2024</p>
+          <p>© {new Date().getFullYear()}</p>
           <p>O’BAO</p>
         </div>
         <div className="flex gap-7 text-xl">
