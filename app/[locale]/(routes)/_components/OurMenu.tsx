f0ae352abf857403d6a0d278cade2a15dbd540af
Xayalxudiyev <xayal.xuduyev@gmail.com>
2025-04-19 14:29:59 +0400

    Enhance OurMenu component with animated text transitions and improved localization: implement automatic text cycling for menu descriptions, update localization keys for better clarity, and refine layout for a more engaging user experience.

--- app/[locale]/(routes)/_components/OurMenu.tsx
+++ app/[locale]/(routes)/_components/OurMenu.tsx
@@ -2,12 +2,31 @@
 import { Button } from "@/components/ui/button"
 import { useTranslations } from "next-intl"
 import Image from "next/image"
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { motion, AnimatePresence } from "framer-motion"
 
 const OurMenuComponent = () => {
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const animates = [
+    "OurMenuAuthentic",
+    "OurMenuTraditional",
+    "OurMenuExquisite",
+    "OurMenuDelightful",
+  ]
+
   const t = useTranslations("home")
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prev) => (prev + 1) % animates.length)
+    }, 2500) // Her 2.5 saniyede bir değişsin
+
+    return () => clearInterval(interval)
+  }, [])
+
   return (
-    <div className=" pt-10 lg:pb-0 w-full mx-auto relative lg:pt-20 flex flex-col justify-center items-center">
+    <div className="pt-10 lg:pb-0 w-full mx-auto relative lg:pt-20 flex flex-col justify-center items-center">
       <div className="lg:block hidden">
         <Image
           src="/miniPhotos/ourmenuleftimg.svg"
@@ -24,18 +43,34 @@ const OurMenuComponent = () => {
           className="absolute bottom-5 right-0"
         />
       </div>
-      <div className="text-center w-4/5   ">
-        <h1 className="text-[#FB4444] text-3xl md:text-4xl font-bold font-avenirMedium4 text-center ">
+      <div className="text-center w-4/5">
+        <h1 className="text-[#FB4444] text-3xl md:text-4xl font-bold font-avenirMedium4 text-center">
           {t("OurMenu")}
         </h1>
-        <p className="text-white text-xl md:text-2xl my-8 leading-7 font-avenirRoman3">
-          {t("OurMenuText")}
+
+        <p className="text-white  text-xl md:text-2xl my-8 leading-7 font-avenirRoman3">
+          {t("OurMenuTextDiscover")}
+          <span className="text-[#FB4444] w-[130px]  lg:w-[140px] ml-1 inline-block relative lg:top-[6px] top-2 h-[28px] overflow-hidden ">
+            <AnimatePresence mode="wait">
+              <motion.span
+                key={animates[currentIndex]}
+                initial={{ y: "100%", opacity: 1 }}
+                animate={{ y: "0%", opacity: 1 }}
+                exit={{ y: "-100%", opacity: 1 }}
+                transition={{ duration: 0.3 }}
+                className="absolute left-0 top-0 h-full w-full flex justify-center items-start "
+              >
+                {t(animates[currentIndex])}
+              </motion.span>
+            </AnimatePresence>
+          </span>
+          {t("OurMenuTextFlavors")}
         </p>
         <div className="flex gap-4 justify-center gap-x-8">
           <Button
             size="sm"
             variant="outline"
-            className="bg-transparent px-7 lg:px-4  text-lg md:text-sm border-[1px] border-[#D2B48C] text-[#D2B48C] rounded-none hover:bg-[#D2B48C] hover:text-black"
+            className="bg-transparent px-7 lg:px-4 text-lg md:text-sm border-[1px] border-[#D2B48C] text-[#D2B48C] rounded-none hover:bg-[#D2B48C] hover:text-black"
             onClick={() =>
               window.open(
                 "https://static1.squarespace.com/static/673227ba8e0761016b822fac/t/67a389f7b7a72a4a09312840/1738770940253/Schuman_A4_menu_print_3mm_bleed.pdf",
